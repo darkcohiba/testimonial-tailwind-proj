4b5ae093c26bc4eba992071841438b6e8d0adbe5
Sam Waters <samuel.waters@gmail.com>
2023-11-25 08:37:07 -0700

    finished config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: ['./dist/*.{html,js}'],
   screens: {
-
+    sm:'480px',
+    md:'768px',
+    lg:'976px',
+    xl:'1440px',
   },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
